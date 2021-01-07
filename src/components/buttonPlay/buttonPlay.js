2658686230d0e9ffeb82a2c0107fb493f11c6d16
Natal1a <natalia132@wp.pl>
2021-01-07 20:03:52 +0100

    Update src/components/buttonPlay/buttonPlay.js
    Co-authored-by: Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>

--- src/components/buttonPlay/buttonPlay.js
+++ src/components/buttonPlay/buttonPlay.js
@@ -1,4 +1,7 @@
-export const buttonPlay = (buttonTitle) => {
-    const buttonTemplateHTML = `<button class='gameButton gameButton_play'>${buttonTitle}</button>`;
+export const buttonPlay = (label, onClick) => {
+    const buttonTemplateHTML = `<button class='gameButton gameButton_play'>${label}</button>`;
+   if (onClick) {
+    buttonPlay.addEventListener('click', onClick);
+  }
     document.body.innerHTML = buttonTemplateHTML;
 };

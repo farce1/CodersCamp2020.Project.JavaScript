97601b0d18d64aae0efcc8f4604af09097e22562
olafk <olaf.koziara@gmail.com>
2021-01-09 15:26:25 +0100

    add mocks

--- test/questionGenerator.test.js
+++ test/questionGenerator.test.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 import {
   fetch,
-  generateQuestions,
+  generateAnswers,
   getImag,
   getImage,
   getImageforRightAnswer,
+  getQuestion,
 } from '../src/scripts/questionGenerator';
 describe('question generate function', () => {
   it('should return 4 answers and right answer', async () => {
-    const response = await generateQuestions({
+    const getSpySuccess = jest.spyOn(axios, 'get').mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        return resolve({
+          data: { name: 'olaf' },
+        });
+      });
+    });
+    const response = await generateAnswers({
       mode: 'people',
       maxId: 12,
       minId: 1,
@@ -22,8 +30,17 @@ describe('question generate function', () => {
 
 describe('get image for right answer function', () => {
   it('should return base64 string', async () => {
+    const getSpySuccess = jest.spyOn(axios, 'get').mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        return resolve({
+          data: new ArrayBuffer(120),
+        });
+      });
+    });
     const response = await getImageforRightAnswer('people', 4);
+
     let base64reg = /^(?:[a-zA-Z0-9+\/]{4})*(?:|(?:[a-zA-Z0-9+\/]{3}=)|(?:[a-zA-Z0-9+\/]{2}==)|(?:[a-zA-Z0-9+\/]{1}===))$/;
+    expect(typeof response).toBe('string');
     expect(response).toMatch(base64reg);
   });
 });

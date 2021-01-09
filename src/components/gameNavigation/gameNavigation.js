51a6e16e2012bcfc1ee0c716c979bdc5729cadcf
s-justina <jus.sobcz@gmail.com>
2021-01-09 23:03:53 +0100

    create basic-navigation styles

--- src/components/gameNavigation/gameNavigation.js
+++ src/components/gameNavigation/gameNavigation.js
@@ -1,14 +1,21 @@
-export const gameNavigation = (onClick) => {
+export const gameNavigation = (callback) => {
   const body = document.getElementsByTagName('body');
   const navigation = document.createElement('div');
   const gameNavigationTemplateHTML = `
-<button class='nav-btn'>People</button>
+<button class='nav-btn clicked'>People</button>
 <button class='nav-btn'>Vehicles</button>
 <button class='nav-btn'>Starships</button>
 `;
-  if (onClick) {
-    gameNavigation.addEventListener('click', onClick);
+
+  function onClickHandler(e) {
+    const currentElement = e.target;
+    console.log(currentElement);
   }
+
+
+    // gameNavigation.addEventListener('click', onClickHandler);
+
+  navigation.classList.add('navigation');
   navigation.innerHTML = gameNavigationTemplateHTML;
   document.body.appendChild(navigation);
 };

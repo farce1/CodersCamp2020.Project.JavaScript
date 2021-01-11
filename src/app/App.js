74fb1f45a007dd4bd4276d729bfb11de9cbd5b98
s-justina <jus.sobcz@gmail.com>
2021-01-12 00:47:05 +0100

    update menu styles

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,6 @@ import {iconButton} from "../components/iconButton";
 import {buttonPlay} from '../components/buttonPlay/buttonPlay.js'
 import {gameNavigation} from '../components/gameNavigation/gameNavigation.js'
 export const App = ({options}) => {
-  // buttonPlay(`Play the game`);
+  buttonPlay(`Play the game`);
   gameNavigation();
-
 };

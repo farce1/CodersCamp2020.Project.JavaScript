ecf6a0a35d70649a6537eb019860064c36fba07c
Natalia Dębska <natalia.debska98@gmail.com>
2021-01-11 17:59:16 +0100

    add tests

---
+++ test/humanPlayer.test.js
@@ -0,0 +1,17 @@
+const humanPlayer = require('../src/domain/players/HumanPlayer')
+
+test('properly invoke function passed as an argument to askQuestion', () => {
+    expect(humanPlayer().askQuestion(function(){return 2+2})).toBe(4);
+});
+
+test('properly return null when no argument is passed to askQuestion', () => {
+    expect(humanPlayer().askQuestion()).toBe(null);
+});
+
+test('properly invoke function passed as an argument to giveAnswer', () => {
+    expect(humanPlayer().giveAnswer(function(){return 2+2})).toBe(4);
+});
+
+test('properly return null when no argument is passed to giveAnswer', () => {
+    expect(humanPlayer().giveAnswer()).toBe(null);
+});

adad7dc7fc4e9693f423f98588d7365a609d6ccf
s-justina <jus.sobcz@gmail.com>
2021-01-14 23:39:17 +0100

    update html structure of application; add styles of modal

--- src/components/commonModal/CommonModal.js
+++ src/components/commonModal/CommonModal.js
@@ -1,10 +1,20 @@
 import { renderComponentIntoSelectedTagID } from '../../utils/functions';
 
 export const CommonModal = (children, callback)=>{
+
   const modal = document.createElement('div');
+  const app = document.getElementById('app-structure');
   modal.classList.add('modal');
+  app.classList.add('modalEffect');
   let childrenHtmlTemplate = `<div>${children}</div>`;
   modal.innerHTML = childrenHtmlTemplate;
-  modal.addEventListener('click', ()=>callback(modal));
+  modal.addEventListener('click', ()=>{
+    callback(modal);
+    removeModalEffect(app, 'modalEffect');
+  });
   return modal
+};
+
+const removeModalEffect = (tag, classToRemove)=>{
+  tag.classList.remove(classToRemove)
 };

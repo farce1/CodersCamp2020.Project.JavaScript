62ae2224910286d9b53256f9d2ea006248bfcdb9
Natalia <natalia132@wp.pl>
2021-01-16 12:19:37 +0100

    conflicts fixed?

--- src/app/App.js
+++ src/app/App.js
@@ -1,52 +1,15 @@
-<< << << < HEAD
-  <<
-  <<
-  << < HEAD
+import {
+  iconButton
+} from '../components/iconButton';
 import {
   buttonPlay
-} from '../components/buttonPlay/ButtonPlay.js'
+} from '../components/buttonPlay/ButtonPlay.js';
+import {
+  gameNavigation
+} from '../components/gameNavigation/gameNavigation.js';
 
 export const App = ({
-    options
-  }) => {
-    buttonPlay(`Play the game`);
-  } ===
-  ===
-  =
-  import {
-    iconButton
-  } from "../components/iconButton";
-export const App = ({
-    options
-  }) => {
-    ===
-    ===
-    =
-    import {
-      iconButton
-    } from '../components/iconButton';
-    import {
-      buttonPlay
-    } from '../components/buttonPlay/ButtonPlay.js';
-    import {
-      gameNavigation
-    } from '../components/gameNavigation/gameNavigation.js'; >>>
-    >>>
-    > origin
-
-    export const App = ({
-      options
-    }) => {
-      gameNavigation();
-    }; <<
-    <<
-    << < HEAD
-
-
-      >>>
-      >>>
-      > 8e4848 ff5dc1be5be01b223df7fe080309a51660 ===
-      ===
-      = >>>
-      >>>
-      > origin
+  options
+}) => {
+  gameNavigation();
+};

f839dfabb63f179151c4e22d5fdd26dce3909f7f
miron54 <miron54@op.pl>
2021-01-16 23:09:36 +0100

    texts fix

--- test/components/iconButton.test.js
+++ test/components/iconButton.test.js
@@ -14,7 +14,7 @@ describe('iconButton', () => {
         
         const component = iconButton(text);
 
-        expect(component.innerText).toEqual(text);
+        expect(component.innerHTML).toEqual(text);
     });
 
     it('should show icon when given icon property', () => {
@@ -28,10 +28,9 @@ describe('iconButton', () => {
 
     it('should not show icon when icon property not provided', () => {
         const text = 'label';
-        
+
         const component = iconButton(text);
-        
-        expect(component.firstChild).toBeNull();
+        expect(component.firstChild).not.toBe('span')
     });
 
     it('should invoke onClick function', () => {

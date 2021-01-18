68a4305d4ad2b52230b3430fc35bbb79a4145d57
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2021-01-18 19:52:25 +0100

    Show component in main View

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
-import { gameNavigation } from '../components/gameNavigation/gameNavigation.js';
+import { gameNavigation } from '../components/gameNavigation/gameNavigation';
+import { modeRules } from '../components/modeRules/modeRules';
 import {
   renderComponentIntoSelectedTagID,
 } from '../utils/functions';
@@ -7,10 +8,16 @@ import { QuestionPanel } from '../components/questionPanel/QuestionPanel';
 const questionPanel = new QuestionPanel();
 
 export const App = ({ options }) => {
-  renderComponentIntoSelectedTagID(gameNavigation(questionPanel), 'swquiz-header');
+  renderComponentIntoSelectedTagID(
+    gameNavigation(questionPanel), 
+    'swquiz-header');
   renderComponentIntoSelectedTagID(
     questionPanel.render(),
-    'swquiz-app',
+    'swquiz-app'
+  );
+  renderComponentIntoSelectedTagID(
+    modeRules(),
+    'swquiz-app'
   );
     // renderComponentIntoSelectedTagID - funkcja do renderowania komponentu w odpowiednim miejscu np.:
   // renderComponentIntoSelectedTagID(gameNavigation(()=>{console.log('tu podaję callback')}), 'swquiz-header');

cd3656824ac9062f09215f86ca2fe223b7362134
Natalia Dębska <natalia.debska98@gmail.com>
2021-01-18 11:34:56 +0100

    fix tests

--- test/AIPlayer.test.js
+++ test/AIPlayer.test.js
@@ -3,22 +3,7 @@ import AIPlayer from '../src/domain/players/AIPlayer';
 describe('askQuestion', () => {
     it('should return null when no argument is passed', () => {
         expect(AIPlayer().askQuestion()).toBe(null);
-    }); 
-});
-
-describe('giveAnswer', () => {
-    it('should invoke function passed as an argument', () => { 
-    expect(AIPlayer().giveAnswer(function(){return 2+2})).toBe(4);
     });
-});
-
-describe('giveAnswer', () => {
-    it('should return null when no argument is passed', () => {
-        expect(AIPlayer().giveAnswer()).toBe(null);
-    }); 
-});
-
-describe('askQuestion', () => {
     it('should invoke function passed as an argument and return object contain id and name property', () => { 
         const anserw = AIPlayer().askQuestion(() => {
             return {
@@ -35,3 +20,13 @@ describe('askQuestion', () => {
         expect(anserw.name).not.toBe(null);
         });
 });
+
+describe('giveAnswer', () => {
+    it('should invoke function passed as an argument', () => { 
+    expect(AIPlayer().giveAnswer(function(){return 2+2})).toBe(4);
+    });
+    it('should return null when no argument is passed', () => {
+        expect(AIPlayer().giveAnswer()).toBe(null);
+    }); 
+});
+

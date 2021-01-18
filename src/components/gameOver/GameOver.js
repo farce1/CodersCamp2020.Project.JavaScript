c4aca4224d5eb7f14b9b1d6e0cef5d665304c083
Natalia <natalia132@wp.pl>
2021-01-18 21:01:31 +0100

    update

--- src/components/gameOver/GameOver.js
+++ src/components/gameOver/GameOver.js
@@ -2,8 +2,12 @@ import {
     buttonPlay
 } from '../buttonPlay/buttonPlay.js';
 import {
-    answersSummary
-} from '../gameOver/answersTemplate.js'
+    quizSummary
+} from './testAnswers';
+import {
+    isCorrectAnswer
+} from '../../domain/functions/isCorrectAnswer/isCorrectAnswer.js'
+
 
 const listItem = (image, correctAnswer, userAnswer, computerAnswer) => {
     const itemTemplate = document.createElement('li');
@@ -22,14 +26,16 @@ const listItem = (image, correctAnswer, userAnswer, computerAnswer) => {
     return itemTemplate;
 }
 
-const generateItemsList = (answersSummary, component) => {
-    answersSummary.forEach(el => {
+const generateItemsList = (quizSummary, component) => {
+    quizSummary.forEach(el => {
         component.querySelector('ul').appendChild(listItem(el.image, el.correct, el.user, el.computer));
     });
 }
 
 export const gameOver = (userAnswers, computerAnswers, onClick) => {
     let component = document.createElement('div');
+    let userQuestionsCount = userAnswers.length;
+    let computerQuestionsCount = computerAnswers.length;
     let correctUserAnswers = userAnswers.filter(answer => answer.isCorrect);
     let correctComputerAnswers = computerAnswers.filter(answer => answer.isCorrect);
 
@@ -38,14 +44,14 @@ export const gameOver = (userAnswers, computerAnswers, onClick) => {
     <div class="game-over__info">
         <h1>GAME OVER</h1>
         <p>The force is strong in you young Padawan! 
-        During 1 minute you have answered ${correctUserAnswers.length} / 23 questions. And Google quessed ${correctComputerAnswers.length} / 23</p>
+        During 1 minute you have answered ${correctUserAnswers.length} / ${userQuestionsCount} questions. And Google quessed ${correctComputerAnswers.length} / ${computerQuestionsCount}</p>
     </div>
     <div class="game-over__answers">
         <img class="yodaImg" src="../../../static/assets/ui/MasterYodaLeft.png">
         
             <div class="answers__list">
             <ul>
-                <li class="answers__list-item list__head"><span></span><span>Answer</span><span>You</span><span>Computer</span></li>
+                <li class="answers__list-item list__head"><span></span><span>Correct</span><span>You</span><span>Computer</span></li>
             </ul>
         </div>
     </div>
@@ -53,8 +59,46 @@ export const gameOver = (userAnswers, computerAnswers, onClick) => {
         <input type="text" id="name" name ="name" required>
         <label for"name">Please fill your name in order to receive eternal glory in whole Galaxy!</label>
     </form>`
+
     component.querySelector('form').appendChild(buttonPlay('MAY THE FORCE BE WITH YOU!'), onClick);
-    generateItemsList(answersSummary, component)
+
+    generateItemsList(quizSummary, component);
+
+
+    component.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        let inputValue = document.querySelector("input").value;
+        let userScore = correctUserAnswers.length;
+        onClick(inputValue, userScore);
+        component.parentElement.removeChild(component);
+    });
+
 
     return component;
+}
+
+
+export const userAnswers = (quizSummary) => {
+    let answers = [];
+    quizSummary.forEach(q => {
+        let playerAnswer = q.user;
+        let correctAnswer = q.correct
+        answers.push({
+            'answer': q.user,
+            'isCorrect': isCorrectAnswer(playerAnswer, correctAnswer)
+        });
+    });
+    return answers;
+}
+export const computerAnswers = (quizSummary) => {
+    let answers = [];
+    quizSummary.forEach(q => {
+        let playerAnswer = q.computer;
+        let correctAnswer = q.correct
+        answers.push({
+            'answer': q.user,
+            'isCorrect': isCorrectAnswer(playerAnswer, correctAnswer)
+        });
+    });
+    return answers;
 }

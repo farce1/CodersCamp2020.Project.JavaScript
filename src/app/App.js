25b96b9b945cc5b94f448abb554323ab6cad6c4b
s-justina <jus.sobcz@gmail.com>
2021-01-18 19:53:45 +0100

    fix conflicts

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import { gameNavigation } from '../components/gameNavigation/gameNavigation.js';
 import { TimerText } from '../components/timerTxt/TimerText';
-import { renderComponentIntoSelectedTag } from '../utils/functions';
+import { renderComponentIntoSelectedTagID } from '../utils/functions';
 import {
   renderComponentIntoSelectedTagID,
 } from '../utils/functions';
@@ -9,13 +9,10 @@ import { QuestionPanel } from '../components/questionPanel/QuestionPanel';
 const questionPanel = new QuestionPanel();
 
 export const App = ({ options }) => {
-  renderComponentIntoSelectedTag(  TimerText(), 'swquiz-header');
+  renderComponentIntoSelectedTagID(  TimerText(), 'swquiz-header');
   renderComponentIntoSelectedTagID(gameNavigation(questionPanel), 'swquiz-header');
   renderComponentIntoSelectedTagID(
     questionPanel.render(),
     'swquiz-app',
   );
-    // renderComponentIntoSelectedTagID - funkcja do renderowania komponentu w odpowiednim miejscu np.:
-  // renderComponentIntoSelectedTagID(gameNavigation(()=>{console.log('tu podaję callback')}), 'swquiz-header');
-  // renderComponentIntoSelectedTagID(CommonModal('kotek', hideElement).show(), 'swquiz-app');
 };

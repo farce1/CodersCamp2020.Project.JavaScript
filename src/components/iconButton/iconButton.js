41c782603640e1e9d8865e79ae1931222566248c
s-justina <jus.sobcz@gmail.com>
2021-01-19 16:58:42 +0100

    remove log and resolve conflicts

--- src/components/iconButton/iconButton.js
+++ src/components/iconButton/iconButton.js
@@ -1,28 +1,27 @@
-const defaultFunction = () => console.log('default');
+const defaultFunction = () => {};
 
-export const iconButton = (text = 'text', icon, onClick = defaultFunction)=> {
+export const iconButton = (text = 'text', icon, onClick = defaultFunction) => {
+  const component = document.createElement('button');
 
-    const component = document.createElement('button');
+  component.className = 'iconButton';
+  component.onclick = onClick;
 
-    component.className = 'iconButton';
-    component.onclick = onClick;
-
-    if (icon) {
+  if (icon) {
     const spanElement = document.createElement('span');
     spanElement.className = 'button__icon';
     spanElement.style.backgroundImage = `url("../static/assets/ui/icons/icon-${icon}_24px.png")`;
     component.appendChild(spanElement);
-    }
-    component.innerHTML += text;
+  }
+  component.innerHTML += text;
 
-    const btn = {
-        render() {
-            return component;
-        },
-        
-        hide() {
-            component.style.display = 'none';
-        }
-    }
-    return btn;
+  const btn = {
+    render() {
+      return component;
+    },
+
+    hide() {
+      component.style.display = 'none';
+    },
+  };
+  return btn;
 };
